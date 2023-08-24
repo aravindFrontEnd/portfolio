0484f4f1beed646c77cc2eba6b6e0d09bf493251
Aravind Ganesh <91867114+Epsilon-Aravind@users.noreply.github.com>
2023-08-24 21:37:58 +0530

    3d-portfolio

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_pyvtiki',
+        'template_swnvre8'
         {
           from_name: form.name,
           to_name: 'Aravind Ganesh',
